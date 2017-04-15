eb40f9ce4394c2c61a113901985082f012d80978
王一烨 <627947015@qq.com>
2017-04-15 17:57:04 +0800

    Update bintrayUpload.sh

--- bintrayUpload.sh
+++ bintrayUpload.sh
@@ -1,1 +1,1 @@
-./gradlew clean build bintrayUpload -PbintrayUser=wwwj -PbintrayKey=fdf0f8221fc90150d1f83feea045698a031c4769 -PdryRun=false
+./gradlew clean build bintrayUpload -PbintrayUser=wwwj -PbintrayKey=*** -PdryRun=false
